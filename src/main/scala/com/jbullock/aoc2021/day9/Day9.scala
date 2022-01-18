02a96f7ed85bb11dc962412451889baa02fc8537
Jason Bullock <notjason7@gmail.com>
2022-01-18 09:43:29 +0000

    WIP

--- src/main/scala/com/jbullock/aoc2021/day9/Day9.scala
+++ src/main/scala/com/jbullock/aoc2021/day9/Day9.scala
@@ -13,11 +13,12 @@ case class Matrix(val matrix: Vector[Vector[Int]]):
   val xRange = (0 until xMax)
   val yMax = matrix.length
   val yRange = (0 until yMax)
-  def safeGet(x: Int, y: Int): Option[Int] = (x, y) match
-    case (x, y) if 0 <= x && x < matrix(0).length && 0 <= y && y < matrix.length => Some(matrix(y)(x))
+  def get(pos: Position): Int = matrix(Position.y)(Position.x)
+  def safeGet(pos: Position): Option[Int] = pos match
+    case Position(x, y) if 0 <= x && x < matrix(0).length && 0 <= y && y < matrix.length => Some(matrix(y)(x))
     case _ => None
 
-case class Coordinate(x: Int, y: Int) 
+case class Position(x: Int, y: Int) 
 
 
 object Day9:
@@ -32,27 +33,47 @@ object Day9:
     val localMinValues = for
       x <- (0 until matrix.xMax)
       y <- (0 until matrix.yMax)
-      if isCoordinateLocalMin(matrix, x, y)
+      if isPositionLocalMin(matrix, x, y)
     yield matrix.safeGet(x, y).get
-    println(localMinValues)
     val minTotal = localMinValues.map(_+1).sum
     println(s"Part one: $minTotal")
   
   def part2(): Unit =
-    ???
+    val matrix = Matrix(input)  
+    val localMinValues = for
+      x <- (0 until matrix.xMax)
+      y <- (0 until matrix.yMax)
+      val pos = Position(x, y)
+      if isPositionLocalMin(matrix, pos)
+    yield matrix.get(pos)
 
-  def isCoordinateLocalMin(matrix: Matrix, x: Int, y: Int): Boolean =
+  def isPositionLocalMin(matrix: Matrix, pos: Position): Boolean =
 
-    def compare(point: Int, comparison: Option[Int]): Boolean = 
+    def compare(point: Int, comparison: Position): Boolean = 
+      val 
       comparison match
         case Some(comparisonInt) => point < comparisonInt
         case None => true
 
-    val point = matrix.matrix(y)(x)
-    val up = compare(point, matrix.safeGet(x, y-1))
-    lazy val down = compare(point, matrix.safeGet(x, y+1))
-    lazy val left = compare(point, matrix.safeGet(x-1, y))
-    lazy val right = compare(point, matrix.safeGet(x+1, y))
+    val posVal = matrix.get(pos)
+    val up = Position(pos.x, pos.y+1)
+    val down = Position(pos.x, pos.y-1)
+    val left = Position(pos.x-1, pos.y)
+    val right = Position(pos.x+1, pos.y)
+
+    val upVal = matrix.safeGet(up)
+    val downVal = matrix.safeGet(down)
+    val leftVal = matrix.safeGet(left)
+    val rightVal = matrix.safeGet(right) 
+
+    val up = compare(posVal, matrix.safeGet(x, y-1))
+    val down = compare(posVal, matrix.safeGet(x, y+1))
+    val left = compare(posVal, matrix.safeGet(x-1, y))
+    lazy val right = compare(posVal, matrix.safeGet(x+1, y))
     up && down && left && right
+
+  // def findBasinSize(matrix: Matrix, localX: Int, localY: Int): Int =
+
+  //   def loop(search)
 
     
