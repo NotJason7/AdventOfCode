8d60565391315b9811e54f48815504bf311d6ccb
Jason Bullock <jasonbullock@ovo-fvfgv3pnq05n.mynet>
2022-01-20 13:57:53 +0000

    Day10
    - Finished Day10

--- src/main/scala/com/jbullock/aoc2021/day10/Day10.scala
+++ src/main/scala/com/jbullock/aoc2021/day10/Day10.scala
@@ -6,14 +6,17 @@ import scala.io.Source
 @main
 def runDay10(): Unit =
   Day10.part1()
-//   Day10.part2()
+  Day10.part2()
+
 
 object Day10:
   val input = Source
-    .fromResource("2021/Day10TestInput.txt")
+    .fromResource("2021/Day10Input.txt")
     .getLines
     .toList
 
+  val openers = Set('{','[','<','(')
+  val closers = Set('}',']','>',')')
   val bracketPairs = Map(
     '{' -> '}',
     '}' -> '{',
@@ -24,52 +27,58 @@ object Day10:
     '(' -> ')',
     ')' -> '(',
   )
+  val syntaxScore = Map(
+    ')' -> 3,
+    ']' -> 57,
+    '}' -> 1197,
+    '>' -> 25137
+  )
+  val syntaxScorePart2 = Map(
+    ')' -> 1,
+    ']' -> 2,
+    '}' -> 3,
+    '>' -> 4
+  )
 
   def part1(): Unit =
     val charLists = input.map(_.toList)
-    val list0 = charLists(2)
-    println(list0)
-    val test = checkSyntax(list0)
-    println(test)
+    val errors = charLists.map(checkSyntax(_)._1)
+    val errorScore = errors.map(syntaxScore.getOrElse(_, 0)).sum
+    println(s"Part one: $errorScore")
+
+  def part2(): Unit =
+    val charLists = input.map(_.toList)
+    val incompleteListValues = charLists
+      .map(checkSyntax(_))
+      .filter(_._1 == ' ')
+      .map(_._2)
+      .map(_.map(syntaxScorePart2.getOrElse(_,0)))
+    println(incompleteListValues)
+    val scores = incompleteListValues.map{
+      values => values.foldLeft(0L)((a, x) => (a * 5) + x)
+    }.sorted
+    println(scores)
+    val winner = scores((scores.length/2))
+    println(winner)
     
-  def checkSyntax(charList: List[Char]): Char =
+  def checkSyntax(chars: List[Char]): (Char, List[Char]) =
+    val noExpectation = ' '
+    val emptyStack = List[Char]()
       
     @tailrec
-    def loop(charList: List[Char], openedCharMap: Map[Char, Int]): Char =
-      charList match
+    def loop(chars: List[Char], expected: Char, stack: List[Char]): (Char, List[Char]) =
+      chars match
         case Nil =>
-          println("somehowgotanil")
-          ' '
+          (noExpectation, stack.prepended(expected).dropRight(1))
         case head :: tail =>
-          println(s"head is $head")
-          val newCharMap = updateMap(head, openedCharMap)
-          newCharMap match
-            case None => head
-            case Some(map) =>
-              loop(tail, map)
-    
-    def updateMap(char: Char, openedCharMap: Map[Char, Int]): Option[Map[Char, Int]] =
-      char match
-        case '{' | '[' | '<' | '(' =>
-          println(s"char $char is an opener")
-          val newValue = openedCharMap.getOrElse(char, 0) + 1
-          val newMap = openedCharMap + (char -> newValue)
-          println(newMap)
-          Some(openedCharMap + (char -> newValue))
-        case '}' | ']' | '>' | ')' =>
-          println(s"char $char is a closer")
-          val openingChar = bracketPairs.get(char)
-          openingChar match
-            case None => None
-            case Some(opener) =>
-              val newValue = openedCharMap.getOrElse(opener, 0) - 1
-              if newValue == -1 then
-                println(s"char $char was BAD")
-                None
-              else  
-                val newMap = openedCharMap + (opener -> newValue)
-                println(newMap)
-                Some(newMap)
-    
-    loop(charList, Map())
-          
+          if openers contains head then
+            loop(tail, bracketPairs.getOrElse(head, noExpectation), stack.prepended(expected))
+          else if closers contains head then
+            if head == expected then
+              loop(tail, stack.headOption.getOrElse(noExpectation), stack.drop(1))
+            else
+              (head, stack.dropRight(1))
+          else
+            (noExpectation, stack.dropRight(1))
+
+    loop(chars, noExpectation, emptyStack)
