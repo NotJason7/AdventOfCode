b1f6c82b50096600082df0da49709c3f0ae05499
Jason Bullock <jasonbullock@ovo-fvfgv3pnq05n.mynet>
2022-02-07 10:45:07 +0000

    Day13\n\n- Formatted Day13

---
+++ src/main/scala/com/jbullock/aoc2021/day13/Puzzle.scala
@@ -0,0 +1,15 @@
+package com.jbullock.aoc2021.day13
+
+import scala.io.Source
+
+def solvePuzzle(): Unit =
+  Puzzle.part1()
+  Puzzle.part2()
+
+object Puzzle {
+
+  def part1(): Unit = ???
+
+  def part2(): Unit = ???
+
+}
