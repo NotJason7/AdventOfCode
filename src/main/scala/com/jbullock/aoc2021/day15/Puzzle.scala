8cadafe9afdd7986f8eb76051ed85b3804a77a9b
Jason Bullock <notjason7@gmail.com>
2022-02-28 19:20:10 +0000

    Day15
    - Did part 1 and am on the road to part 2.

--- src/main/scala/com/jbullock/aoc2021/day15/Puzzle.scala
+++ src/main/scala/com/jbullock/aoc2021/day15/Puzzle.scala
@@ -1,7 +1,9 @@
 package com.jbullock.aoc2021.day15
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.io.Source
+import scala.collection.mutable.PriorityQueue
 
 @main
 def solvePuzzle(): Unit =
@@ -9,10 +11,10 @@ def solvePuzzle(): Unit =
     .fromResource("2021/Day15/Example.txt")
     .getLines
     .toList
-  val part1Answer = Puzzle.part1(input)
-  println(s"Part1: $part1Answer")
-//  val part2Answer = Puzzle.part2(input)
-//  println(s"Part2: $part2Answer")
+//  val part1Answer = Puzzle.part1(input)
+//  println(s"Part1: $part1Answer")
+  val part2Answer = Puzzle.part2(input)
+  println(s"Part2: $part2Answer")
 
 case class Point(x: Int, y: Int)
 extension(p: Point)
@@ -24,22 +26,48 @@ extension(p: Point)
       (0, 1)
     )
     val potentialPoints = transformations.map(t => Point(t._1 + p.x, t._2 + p.y))
-    val validPoints = potentialPoints.filter(cavern.positions.contains)
+    val validPoints = potentialPoints.filter(cavern.points.contains)
     validPoints
-  def manhattanDistance(target: Point): Int =
-    math.abs(p.x - t.x) + math.abs(p.y - t.y)
+  def heuristic(using target: Point): Int =
+    math.abs(p.x - target.x) + math.abs(p.y - target.y)
   def risk(using cavern: Cavern): Int =
-    cavern.positions(p)
+    cavern.points(p)
+  def score(using target: Point, cavern: Cavern): Int =
+    cavern.points(p) + p.heuristic
 
-case class Node(point: Point, path: Path)
+case class Node(point: Point, parent: Option[Node], fitness: Int, risk: Int)
 extension(n: Node)
-  def risk: Int =
-    n.path.risk
-  def extensions: List[Node] =
-    n.point.adjacentPoints.map(p => Node(p, n.path.extend(p)))
+  def adjacentNodes(using target: Point, cavern: Cavern): List[Node] =
+    n.point.adjacentPoints.map{p =>
+      Node(p, Some(n), n.risk + p.risk + p.heuristic, n.risk + p.risk)
+    }
+  def buildPath: Path =
+
+    @tailrec
+    def loop(list: List[Node]): List[Node] =
+      val current = list.head
+      current.parent match
+        case None => list
+        case Some(parent) => loop(list.prepended(parent))
+
+    Path(loop(List(n)))
+
+given Ordering[Node] with
+  def compare(a: Node, b: Node): Int = b.fitness.compare(a.fitness)
+
+case class Path(nodes: List[Node])
+object Path:
+  def fromNode(n: Node): Path =
+    Path(List(n))
+extension(p: Path)
+  def extend(n: Node): Path =
+    Path(p.nodes.prepended(n))
+  def write(): Unit =
+    println(p.nodes.reverse.map(n => s"(${n.point.x},${n.point.y}): ${n.risk}").mkString("\n"))
 
 
-case class Cavern(positions: Map[Point, Int])
+
+case class Cavern(points: Map[Point, Int])
 object Cavern:
   def fromList(l: List[String]): Cavern =
     val yRange = l.indices
@@ -50,52 +78,78 @@ object Cavern:
       x <- l.head.indices
     } yield Map(Point(x, y) -> positions(y)(x))
     Cavern(cavern.flatten.toMap)
+  def fromListBigVersion(l: List[String]): Cavern =
+    val yRange = l.indices
+    val width = l.head.length
+    val height = l.size
+    val xRange = l.head.indices
+    val positions = l.map(_.toList.map(_.asDigit))
+    val cavern = for {
+      xFactor <- (0 to 4).toList
+      x <- l.head.indices
+      yFactor <- (0 to 4).toList
+      y <- l.indices
+    } yield Map(Point(x + (xFactor * width), y + (yFactor * height)) -> (positions(y)(x) + (xFactor + yFactor)))
+    Cavern(cavern.flatten.toMap)
 
-case class Path(visited: List[Point], risk: Int)
-object Path:
-  def fromPoint(p: Point)(using cavern: Cavern): Path =
-    Path(List(p), p.risk)
-extension(p: Path)
-  def extend(point: Point)(using cavern: Cavern): Path =
-    val currentRisk = p.risk
-    val pointRisk = point.risk
-    println(s"Next risk is ${currentRisk+pointRisk} ( current is $currentRisk and point is $pointRisk)")
-    Path(point :: p.visited, point.risk + point.risk)
+
+
+def aStar(start: Point)(using target: Point, cavern: Cavern): Path =
+
+  @tailrec
+  def loop(openList: mutable.PriorityQueue[Node], closedList: Set[Point])(using target: Point, cavern: Cavern): Path =
+
+    val current = openList.dequeue()
+
+    if current.point == target then
+      current.buildPath
+    else
+      val newNodes = current.adjacentNodes.filterNot(node => closedList.contains(node.point))
+      val overlap = openList.filter(existing =>
+        newNodes.map(_.point).contains(existing.point)).toList
+      val newOpenList = openList.filterNot(overlap.contains(_))
+      val newOverlap = newNodes.filter(node => overlap.map(_.point).contains(node.point))
+      val newUnseen = newNodes.filterNot(node => overlap.map(_.point).contains(node.point))
+      newUnseen.foreach(newOpenList.enqueue(_))
+      val bestOverlaps = overlap.map(oldNode =>
+        val newNode = newOverlap.filter(_.point == oldNode.point).head
+        if newNode.fitness < oldNode.fitness then newNode else oldNode
+      )
+      bestOverlaps.foreach(newOpenList.enqueue(_))
+
+      val newClosedList = closedList + current.point
+      loop(newOpenList, newClosedList)
+
+  val startNode = mutable.PriorityQueue[Node](Node(start, None, start.heuristic, 0))
+
+  loop(startNode, Set.empty)
 
 object Puzzle:
 
   def part1(input: List[String]): Int =
     given cavern: Cavern = Cavern.fromList(input)
+    given target: Point = Point(input.head.length-1, input.size-1)
     val start = Point(0, 0)
-    val end = Point(input.head.length-1, input.size-1)
-    val path = pathFind(start, end)
-    println(path)
-    path.risk
+    val path = aStar(start)
+    path.write()
+    val risk = path.nodes.head.risk
+    risk
 
-  def part2(input: List[String]): Int = ???
+  def part2(input: List[String]): Int =
+    given cavern: Cavern = Cavern.fromListBigVersion(input)
+    val points = for {
+      x <- (0 to 49).toList
+      y <- (0 to 49).toList
+    } yield Point(x, y)
+    println(points.map(cavern.points(_)).mkString)
+    1
+//    given target: Point = Point(input.head.length*5-1, input.size*5-1)
+//    val start = Point(0, 0)
+//    val path = aStar(start)
+//    path.write()
+//    val risk = path.nodes.head.risk
+//    risk
 
-  def searchNode(currentNode: Node, visitedNodes: List[Node], target: Point)(using cavern: Cavern): Path =
 
 
-//  def buildPaths(current: Path, target: Point)(using cavern: Cavern): List[Path] =
-//    val head = current.visited.head
-//    val nextSteps = head.adjacentPoints
-//    if nextSteps.contains(target) then
-//      List(current.extend(target))
-//    nextSteps.collect{ case point: Point if !current.visited.contains(point) =>
-//      current.extend(point)
-//    }
-//
-//  def pathFind(start: Point, target: Point)(using cavern: Cavern): Path =
-//
-//    @tailrec
-//    def loop(sortedPaths: List[Path], target: Point): Path =
-//      val completePaths = sortedPaths.filter(_.visited.contains(target))
-//      if completePaths.nonEmpty then completePaths.head
-//      else
-//        val nextPaths = sortedPaths.flatMap(p => buildPaths(p, target))
-//        val sortedNextPaths = nextPaths.sortWith(_.risk > _.risk)
-//        loop(sortedNextPaths, target)
-//
-//    val startPath = List(Path.fromPoint(start))
-//    loop(startPath, target)
+
--- src/test/scala/com/jbullock/aoc2021/day15/PuzzleSpec.scala
+++ src/test/scala/com/jbullock/aoc2021/day15/PuzzleSpec.scala
@@ -1,20 +1,20 @@
-package com.jbullock.aoc2021.day15
-
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.must.Matchers
-
-import scala.io.Source
-
-class PuzzleSpec extends AnyFlatSpec with Matchers {
-  val example: List[String] = Source
-    .fromResource("2021/Day15/Example.txt")
-    .getLines
-    .toList
-
-  "Part1" must "give correct answer for example" in {
-    val answer = Puzzle.part1(example)
-    val expected = 40
-    answer mustBe expected
-  }
-
-}
+//package com.jbullock.aoc2021.day15
+//
+//import org.scalatest.flatspec.AnyFlatSpec
+//import org.scalatest.matchers.must.Matchers
+//
+//import scala.io.Source
+//
+//class PuzzleSpec extends AnyFlatSpec with Matchers {
+//  val example: List[String] = Source
+//    .fromResource("2021/Day15/Example.txt")
+//    .getLines
+//    .toList
+//
+//  "Part1" must "give correct answer for example" in {
+//    val answer = Puzzle.part1(example)
+//    val expected = 40
+//    answer mustBe expected
+//  }
+//
+//}
