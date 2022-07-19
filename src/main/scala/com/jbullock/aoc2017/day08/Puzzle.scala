3af9278afc7878ad704da387bb5795bf249fbf31
Jason Bullock <notjason7@gmail.com>
2022-07-19 12:08:00 +0100

    Finished day08

--- src/main/scala/com/jbullock/aoc2017/day08/Puzzle.scala
+++ src/main/scala/com/jbullock/aoc2017/day08/Puzzle.scala
@@ -8,19 +8,29 @@ def solvePuzzle(): Unit =
   println(s"Part 2: ${Puzzle.part2}")
 
 object Puzzle:
-  val input: Vector[Instruction] = Source.fromResource("aoc/2017/Day08/Example.txt").getLines.toVector.map(Instruction
+  val input: Vector[Instruction] = Source.fromResource("aoc/2017/Day08/Input.txt").getLines.toVector.map(Instruction
     .fromString)
-  println(input)
-  val output: State = input.foldLeft(State(0, Map.empty[String, Int]))((s, i) => i.makeChange(s))
-  println(output)
-
+  val output: State = input.foldLeft(State(0, Map.empty[String, Int]))((s, i) => i.followInstruction(s))
   def part1: Int = output.registers.values.max
   def part2: Int = output.max
 
-
 case class State(max: Int, registers: Map[String, Int])
 
-case class Condition(name: String, test: Int => Boolean)
+case class Change(name: String, value: Int):
+  def evaluate(s: State): State =
+    val next = s.registers.getOrElse(name, 0) + value
+    State(Set(next, s.max).max, s.registers.updated(name, next))
+object Change:
+  def fromString(s: String): Change = s.split(" ", 2).toVector match
+    case Vector(name, update) =>
+      val value = update.split(" ", 2).toVector match
+        case Vector(direction, magnitude) => if direction.startsWith("dec") then -magnitude.toInt else magnitude.toInt
+        case _ => throw new RuntimeException(s"Unable to parse update $update")
+      Change(name, value)
+    case _ => throw new RuntimeException(s"Unable to parse change $s")
+
+case class Condition(name: String, test: Int => Boolean):
+  def evaluate(s: State): Boolean = test(s.registers.getOrElse(name,0))
 object Condition:
   def fromString(s: String): Condition = s.split(" ", 2).toVector match
     case Vector(name, testString) =>
@@ -39,45 +49,10 @@ object Condition:
         case _ => throw new RuntimeException(s"Unable to evaluate boolean test $testString")
     case _ => throw new RuntimeException(s"Unable to evaluate condition $s")
 
-case class Instruction(change: Change, condition: String):
-  def evaluateCondition(state: State): Boolean =
-    condition.split(" ", 2).toVector match
-      case Vector(name, test) =>
-        val register = state.registers.getOrElse(name, 0)
-        test.split(" ", 2).toVector match
-          case Vector(bool, value) =>
-            val comparison = value.toInt
-            bool match
-              case ">" => register > comparison
-              case "<" => register < comparison
-              case ">=" => register >= comparison
-              case "==" => register == comparison
-              case "<=" => register <= comparison
-              case "!=" => register != comparison
-              case _ => throw new RuntimeException(s"Unable to compare using bool $bool")
-          case _ => throw new RuntimeException(s"Unable to evaluate boolean test $test")
-      case _ => throw new RuntimeException(s"Unable to evaluate condition $condition")
-  def makeChange(state: State): State =
-    if evaluateCondition(state) then change.evaluate(state)
-    else state
+case class Instruction(change: Change, condition: Condition):
+  def followInstruction(s: State): State = if condition.evaluate(s) then change.evaluate(s) else s
 object Instruction:
   def fromString(s: String): Instruction =
     s.split(" if ").toVector match
-      case Vector(change, condition) => Instruction(Change.fromString(change), condition)
+      case Vector(change, condition) => Instruction(Change.fromString(change), Condition.fromString(condition))
       case _ => throw new RuntimeException(s"Unable to parse instruction $s")
-
-case class Change(name: String, value: Int):
-  def evaluate(s: State): State =
-    val next = s.registers.getOrElse(name, 0) + value
-    val max = Set(next, s.max).max
-    State(max, s.registers.updated(name, next))
-object Change:
-  def fromString(s: String): Change = s.split(" ", 2).toVector match
-    case Vector(name, update) =>
-      val value = update.split(" ", 2).toVector match
-        case Vector(direction, magnitude) => direction match
-          case "inc" => magnitude.toInt
-          case "dec" => -magnitude.toInt
-        case _ => throw new RuntimeException(s"Unable to parse update $update")
-      Change(name, value)
-    case _ => throw new RuntimeException(s"Unable to parse change $s")
