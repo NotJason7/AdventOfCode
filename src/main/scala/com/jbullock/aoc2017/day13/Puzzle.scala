161bc9e07fa90861a7ec6c6cb7bad9281a1218fc
Jason Bullock <notjason7@gmail.com>
2022-08-29 22:54:15 +0100

    Finished Day15 and did a lil tidyup

--- src/main/scala/com/jbullock/aoc2017/day13/Puzzle.scala
+++ src/main/scala/com/jbullock/aoc2017/day13/Puzzle.scala
@@ -4,16 +4,17 @@ import scala.annotation.tailrec
 import scala.io.Source
 
 @main
-def solvePuzzle(): Unit = Puzzle
+def solvePuzzle(): Unit =
+
+  println(s"Part 1: ${Puzzle.part1}\nPart 2: ${Puzzle.part2}")
 
 object Puzzle:
   given firewall: Vector[Layer] = Source.fromResource("aoc/2017/Day13/Input.txt").getLines.toVector.map(Layer.fromString)
-//  given firewall: Vector[Layer] = Vector("0: 3","1: 2","4: 4","6: 4").map(Layer.fromString)
-  val part1: State = State(0, 0).steps(firewall.takeRight(1).head.depth + 1)
-  println(s"Part 1: $part1")
-  val sieve: Int => Boolean = firewall.foldLeft((x: Int) => true)((f, layer) => (x: Int) => f(x) & (x % layer.depth
-    * 2 == 0))
-  println(sieve(1))
+  def sieve: Int => Boolean = firewall.foldLeft((x: Int) => true){ (f, layer) =>
+    (x: Int) => f(x) && ((x + layer.depth) % ((layer.range-1) * 2) != 0)
+  }
+  val part1: Int = State(0, 0).steps(firewall.last.depth).severity
+  val part2: Int = LazyList.from(0).filter(sieve(_)).head
 
 case class State(picoseconds: Int, severity: Int)(using firewall: Vector[Layer]):
   def step: State = firewall.find(layer => layer.depth == picoseconds && layer.positionAt(picoseconds) == 0) match
@@ -29,10 +30,9 @@ case class State(picoseconds: Int, severity: Int)(using firewall: Vector[Layer])
 case class Layer(depth: Int, range: Int):
   val severity: Int = depth * range
   def positionAt(t: Int): Int =
-    val search: Vector[Int] = ((0 until range) ++ (range - 2 to 1 by -1)).toVector
+    val search = (0 until range) ++ (range - 2 to 1 by -1)
     LazyList.continually(search).to(LazyList).flatten.drop(t).head
 object Layer:
-  def fromString(s: String): Layer =
-    "\\d+".r.findAllIn(s).toVector.map(_.toInt) match
-      case Vector(depth: Int, range: Int) => Layer(depth, range)
-      case _ => throw new RuntimeException(s"Unable to create Layer from ${s}")
+  def fromString(s: String): Layer = "\\d+".r.findAllIn(s).toVector.map(_.toInt) match
+    case Vector(depth: Int, range: Int) => Layer(depth, range)
+    case _ => throw new RuntimeException(s"Unable to create Layer from ${s}")
---
+++ src/main/scala/com/jbullock/aoc2017/day15/Puzzle.scala
@@ -0,0 +1,41 @@
+package com.jbullock.aoc2017.day15
+
+import scala.annotation.tailrec
+
+object Puzzle:
+  @main
+  def solve(): Unit =
+    val part1 = countMatches(State(516, 190, Mode.Normal), 40000000)
+    val part2 = countMatches(State(516, 190, Mode.Picky), 5000000)
+    println(s"Part 1: ${part1}\nPart 2: ${part2}")
+
+extension(l: Long)
+  def toPaddedBinaryString(n: Int): String = l.toBinaryString.reverse.padTo(n, 0).reverse.mkString
+
+enum Mode:
+  case Normal, Picky
+
+case class State(a: Long, b: Long, mode: Mode):
+  val (aFactor, bFactor, divisor) = (16807, 48271, 2147483647)
+
+  def lowest16BitsMatch: Boolean = a.toPaddedBinaryString(32).takeRight(16) == b.toPaddedBinaryString(32).takeRight(16)
+
+  def nextValue(current: Long, factor: Long): Long = (current * factor) % divisor
+
+  def nextValueDivisibleByN(current: Long, factor: Long, n: Int): Long =
+    @tailrec
+    def loop(current: Long): Long =
+      if current % n == 0 then current else loop(nextValue(current, factor))
+
+    loop(nextValue(current, factor))
+
+  def next: State = mode match
+    case Mode.Normal => State(nextValue(a, aFactor), nextValue(b, bFactor), mode)
+    case Mode.Picky  => State(nextValueDivisibleByN(a, aFactor, 4), nextValueDivisibleByN(b, bFactor, 8), mode)
+
+def countMatches(state: State, n: Int): Int =
+  @tailrec
+  def loop(state: State, n: Int, total: Int): Int =
+    if n == 0 then total else loop(state.next, n-1, if state.lowest16BitsMatch then total + 1 else total)
+
+  loop(state, n, 0)
