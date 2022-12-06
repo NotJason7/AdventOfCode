b38b886a08b060f32a02de7b90b4007a8d4f2834
NotJason7 <notjason7@gmail.com>
2022-12-06 10:48:31 +0000

    Minor renaming

--- src/main/scala/com/jbullock/aoc2022/day06/Puzzle.scala
+++ src/main/scala/com/jbullock/aoc2022/day06/Puzzle.scala
@@ -1,11 +1,11 @@
 package com.jbullock.aoc2022.day06
 
 @main def solvePuzzle(): Unit =
-  val input                   = io.Source.fromResource("aoc/2022/Day06/Input.txt").mkString.trim
-  val startOfPacketMarker     = input.markerIndexEnd(4)
-  val realStartOfPacketMarker = input.markerIndexEnd(14)
-  println(s"Part 1: $startOfPacketMarker")
-  println(s"Part 2: $realStartOfPacketMarker")
+  val input         = io.Source.fromResource("aoc/2022/Day06/Input.txt").mkString.trim
+  val packetMarker  = input.markerIndex(4)
+  val messageMarker = input.markerIndex(14)
+  println(s"Part 1: $packetMarker")
+  println(s"Part 2: $messageMarker")
 
 extension (s: String)
-  def markerIndexEnd(markerSize: Int): Int = s.sliding(markerSize).takeWhile(s => s != s.distinct).size + markerSize
+  def markerIndex(markerSize: Int): Int = s.sliding(markerSize).takeWhile(s => s != s.distinct).size + markerSize
