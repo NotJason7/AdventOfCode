fd88790456560bdc21500cc1acc8a0f10e26ec8d
Jason Bullock <notjason7@gmail.com>
2022-12-26 16:15:38 +0000

    Working part 2 again.. still broken part 1 x _ x

--- src/main/scala/com/jbullock/aoc2022/day13/Puzzle.scala
+++ src/main/scala/com/jbullock/aoc2022/day13/Puzzle.scala
@@ -3,30 +3,26 @@ package com.jbullock.aoc2022.day13
 import scala.annotation.tailrec
 
 @main def solvePuzzle(): Unit =
-  val input   = scala.io.Source.fromResource("aoc/2022/Day13/Input.txt").getLines.toVector.filterNot(_.isBlank)
-  val tokens  = input.map(formatSignal)
-  val signals = tokens.grouped(2).toVector.map(v => Signal(v(0), v(1)))
-  val part1   = signals.map(_.isSortedSignal).zipWithIndex.filter(_._1).map(_._2 + 1).sum
+  val input  = scala.io.Source.fromResource("aoc/2022/Day13/Input.txt").getLines.toVector.filterNot(_.isBlank)
+  val tokens = input.map(formatSignal)
+  val sorted = tokens.grouped(2).toVector.map(v => v(0).isSorted(v(1)))
+  val part1  = sorted.zipWithIndex.filter(_._1).map(_._2 + 1).sum
   println(s"Part 1: $part1") //5588
   val divider2           = Vector("[", "[", "2", "]", "]")
   val divider6           = Vector("[", "[", "6", "]", "]")
   val tokensWithDividers = input.map(formatSignal) ++ Vector(divider2, divider6)
-  val sorted             = tokensWithDividers.sortWith(_.isSorted(_))
-  val divider2index      = sorted.indexOf(divider2) + 1
-  val divider6index      = sorted.indexOf(divider6) + 1
+  val sortedWithDividers = tokensWithDividers.sortWith(_.isSorted(_))
+  val divider2index      = sortedWithDividers.indexOf(divider2) + 1
+  val divider6index      = sortedWithDividers.indexOf(divider6) + 1
   val part2              = divider2index * divider6index
-  println(s"Part 2: $part2") //23958
-
-case class Signal(left: Vector[String], right: Vector[String]):
-  def isSortedSignal: Boolean = left.isSorted(right)
+  println(s"Part 2: $part2")
 
 def formatSignal(s: String) = s
   .replace("[", ",[,")
   .replace("]", ",],")
   .split(',')
   .toVector
-  .flatten
-  .map(_.toString)
+  .filterNot(_.isBlank)
 
 extension (leftVector: Vector[String])
   def isSorted(rightVector: Vector[String]): Boolean =
@@ -34,8 +30,8 @@ extension (leftVector: Vector[String])
       (left.headOption, right.headOption) match
         case (Some(l), Some(r)) =>
           if l == r then loop(left.tail, right.tail)
-          else if r == "]" then false
           else if l == "]" then true
+          else if r == "]" then false
           else if l == "[" then loop(left.tail, right)
           else if r == "[" then loop(left, right.tail)
           else l.toInt <= r.toInt
