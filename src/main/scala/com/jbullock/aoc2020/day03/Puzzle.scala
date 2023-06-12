f15e20b4e8f411e1b435be2248cb1893fb2361e4
NotJason7 <notjason7@gmail.com>
2023-06-12 10:45:51 +0100

    Initial Day 3 solution

--- src/main/scala/com/jbullock/aoc2020/day03/Puzzle.scala
+++ src/main/scala/com/jbullock/aoc2020/day03/Puzzle.scala
@@ -1,49 +1,28 @@
 package com.jbullock.aoc2020.day03
 
+import scala.annotation.tailrec
+
 @main def solvePuzzle(): Unit =
-  val input           = scala.io.Source.fromResource("aoc/2020/Day03/Input.txt").getLines.toVector
-  val treeMap = for
-    y <- input.indices
-    x <- 0 until mapWidth
-    tile = input(y)(x)
-  yield Position(x, y) -> tile
-  val part1Trajectory = Trajectory(3, 1)
-  val part1 = input
-    .drop(1)
-    .foldLeft(State(Position.origin, 0)) { case (state: State, mapString: String) =>
-      val newPosition = state.position.followTrajectory(part1Trajectory)
-      val currentTile = mapString(newPosition.x % mapString.length)
-      val hitTree     = if currentTile == '#' then 1 else 0
-//      println(
-//        s"Character ${newPosition.x} (${newPosition.x % mapString.length}) of $mapString: $currentTile, so $hitTree"
-//      )
-      State(newPosition, state.treesHit + hitTree)
-    }
-    .treesHit
+  val input = scala.io.Source.fromResource("aoc/2020/Day03/Input.txt").getLines.toVector
+  val part1 = countHitTrees(input, Trajectory(3, 1))
   println(s"Part 1: $part1")
-  val part2Trajectories = List(
-    Trajectory(1, 1), Trajectory(3, 1), Trajectory(5, 1), Trajectory(7, 1), Trajectory(1, 2)
-  )
+  val part2 = List(
+    Trajectory(1, 1),
+    Trajectory(3, 1),
+    Trajectory(5, 1),
+    Trajectory(7, 1),
+    Trajectory(1, 2)
+  ).map(t => countHitTrees(input, t)).product
+  println(s"Part 2: $part2")
 
 case class Trajectory(right: Int, down: Int)
-case class Position(x: Int, y: Int):
-  def followTrajectory(trajectory: Trajectory): Position = Position(x + trajectory.right, y + trajectory.down)
-  override def toString: String                          = s"($x,$y)"
-object Position:
-  val origin: Position = Position(0, 0)
-case class State(position: Position, treesHit: Int)
-
-//  val mapWidth = input.head.length
-//  val part1Trajectory = 3
-//  val part1 = input.foldLeft((Position.origin, 0)){ case ((position, treesHit), treeLine) =>
-//    val nextPosition =
-//  }
-//enum Tile:
-//  case Tree, Space
-//
-//case class Trajectory(right: Int, down: Int)
-//
-//case class Position(x: Int, y: Int):
-//  def followTrajectory(trajectory: Trajectory): Position = Position(x + trajectory.right, y + trajectory.down)
-//object Position:
-//  val origin: Position = Position(0, 0)
+def countHitTrees(map: Vector[String], trajectory: Trajectory): Int =
+  val repeatWidth = map.head.length
+  @tailrec
+  def loop(currentMap: Vector[String], currentX: Int, trees: Int): Int =
+    if currentMap.isEmpty then trees
+    else
+      val nextX  = (currentX + trajectory.right) % repeatWidth
+      val onTree = if currentMap.head(nextX) == '#' then 1 else 0
+      loop(currentMap.drop(trajectory.down), nextX, trees + onTree)
+  loop(map.drop(trajectory.down), 0, 0)
