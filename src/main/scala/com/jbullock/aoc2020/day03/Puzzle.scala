a0af8175c9d01b01bff3788518d508832e64fcb5
NotJason7 <notjason7@gmail.com>
2023-06-12 11:26:23 +0100

    Tidier day 3

--- src/main/scala/com/jbullock/aoc2020/day03/Puzzle.scala
+++ src/main/scala/com/jbullock/aoc2020/day03/Puzzle.scala
@@ -3,26 +3,22 @@ package com.jbullock.aoc2020.day03
 import scala.annotation.tailrec
 
 @main def solvePuzzle(): Unit =
-  val input = scala.io.Source.fromResource("aoc/2020/Day03/Input.txt").getLines.toVector
-  val part1 = countHitTrees(input, Trajectory(3, 1))
+  val input                 = scala.io.Source.fromResource("aoc/2020/Day03/Input.txt").getLines.toVector
+  given Vector[Vector[Int]] = input.map(_.toVector.map(c => if c == '#' then 1 else 0))
+  val part1                 = countHitTrees(Trajectory(3))
   println(s"Part 1: $part1")
-  val part2 = List(
-    Trajectory(1, 1),
-    Trajectory(3, 1),
-    Trajectory(5, 1),
-    Trajectory(7, 1),
-    Trajectory(1, 2)
-  ).map(t => countHitTrees(input, t)).product
+  val part2 = List(Trajectory(1), Trajectory(3), Trajectory(5), Trajectory(7), Trajectory(1, 2))
+    .map(countHitTrees)
+    .product
   println(s"Part 2: $part2")
 
-case class Trajectory(right: Int, down: Int)
-def countHitTrees(map: Vector[String], trajectory: Trajectory): Int =
-  val repeatWidth = map.head.length
+case class Trajectory(x: Int, y: Int = 1)
+def countHitTrees(t: Trajectory)(using treeMap: Vector[Vector[Int]]): Int =
   @tailrec
-  def loop(currentMap: Vector[String], currentX: Int, trees: Int): Int =
-    if currentMap.isEmpty then trees
-    else
-      val nextX  = (currentX + trajectory.right) % repeatWidth
-      val onTree = if currentMap.head(nextX) == '#' then 1 else 0
-      loop(currentMap.drop(trajectory.down), nextX, trees + onTree)
-  loop(map.drop(trajectory.down), 0, 0)
+  def loop(map: Vector[Vector[Int]], x: Int = 0, trees: Int = 0): Int =
+    map.headOption match
+      case None => trees
+      case Some(treeRow) =>
+        val nextX = (x + t.x) % treeRow.size
+        loop(map.drop(t.y), nextX, trees + treeRow(nextX))
+  loop(treeMap.drop(t.y))
