b4bcca3f4afd0d356611316761fdd1696f358fa7
NotJason7 <notjason7@gmail.com>
2023-06-12 12:21:29 +0100

    Completed Day 3 very tidy

--- src/main/scala/com/jbullock/aoc2020/day03/Puzzle.scala
+++ src/main/scala/com/jbullock/aoc2020/day03/Puzzle.scala
@@ -1,24 +1,14 @@
 package com.jbullock.aoc2020.day03
 
-import scala.annotation.tailrec
-
 @main def solvePuzzle(): Unit =
-  val input                 = scala.io.Source.fromResource("aoc/2020/Day03/Input.txt").getLines.toVector
-  given Vector[Vector[Int]] = input.map(_.toVector.map(c => if c == '#' then 1 else 0))
-  val part1                 = countHitTrees(Trajectory(3))
+  given Vector[String] = scala.io.Source.fromResource("aoc/2020/Day03/Input.txt").getLines.toVector
+  val part1            = Trajectory(3).treesHit
   println(s"Part 1: $part1")
-  val part2 = List(Trajectory(1), Trajectory(3), Trajectory(5), Trajectory(7), Trajectory(1, 2))
-    .map(countHitTrees)
-    .product
+  val part2 = List(Trajectory(1), Trajectory(3), Trajectory(5), Trajectory(7), Trajectory(1, 2)).map(_.treesHit).product
   println(s"Part 2: $part2")
 
-case class Trajectory(x: Int, y: Int = 1)
-def countHitTrees(t: Trajectory)(using treeMap: Vector[Vector[Int]]): Int =
-  @tailrec
-  def loop(map: Vector[Vector[Int]], x: Int = 0, trees: Int = 0): Int =
-    map.headOption match
-      case None => trees
-      case Some(treeRow) =>
-        val nextX = (x + t.x) % treeRow.size
-        loop(map.drop(t.y), nextX, trees + treeRow(nextX))
-  loop(treeMap.drop(t.y))
+case class Position(x: Int, y: Int)
+case class Trajectory(x: Int, y: Int = 1):
+  def positions(width: Int, n: Int): Seq[Position] = (0 until n).map(i => Position((i * x) % width, i * y))
+  def treesHit(using map: Vector[String]): Int =
+    positions(map.head.length, map.size / y).count(p => map(p.y)(p.x) == '#')
