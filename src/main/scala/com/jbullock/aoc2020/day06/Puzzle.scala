db3f9cfa3abd00089755f6c6e240062974c0b136
NotJason7 <notjason7@gmail.com>
2023-06-27 17:51:06 +0100

    Improved day 6

--- src/main/scala/com/jbullock/aoc2020/day06/Puzzle.scala
+++ src/main/scala/com/jbullock/aoc2020/day06/Puzzle.scala
@@ -1,9 +1,9 @@
 package com.jbullock.aoc2020.day06
 
 @main def solvePuzzle(): Unit =
-  val input   = scala.io.Source.fromResource("aoc/2020/Day06/Input.txt").getLines.toVector
-  val surveys = input.mkString(",").split(",,").toVector
-  val part1   = surveys.map(s => s.replace(",", "").toSet).map(_.size).sum
+  val input        = scala.io.Source.fromResource("aoc/2020/Day06/Input.txt").getLines.toVector
+  val surveyGroups = input.mkString(",").split(",,").toVector.map(_.split(',').map(_.toSet))
+  val part1        = surveyGroups.map(_.reduce(_ | _).size).sum
+  val part2        = surveyGroups.map(_.reduce(_ & _).size).sum
   println(s"Part 1: $part1")
-  val part2 = surveys.map(_.split(',').toVector.map(_.toSet).reduce(_.intersect(_)).size).sum
   println(s"Part 2: $part2")
