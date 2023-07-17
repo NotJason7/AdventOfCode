95c6067deba02f9c0c76bbf2c29a07a638f258cd
NotJason7 <notjason7@gmail.com>
2023-07-17 15:25:51 +0100

    day 8 tidy up

--- src/main/scala/com/jbullock/aoc2020/day08/Puzzle.scala
+++ src/main/scala/com/jbullock/aoc2020/day08/Puzzle.scala
@@ -4,35 +4,33 @@ package com.jbullock.aoc2020.day08
   val input                            = scala.io.Source.fromResource("aoc/2020/Day08/Input.txt").getLines.toVector
   val instructions                     = input.flatMap(Instruction.fromString)
   val start                            = BootState.from(instructions)
-  val valueAtFirstDuplicateInstruction = start.followInstructionsUntilDuplicateOrTerminate.valueAtDuplicate.get
-  println(s"Part 1: $valueAtFirstDuplicateInstruction")
+  val valueAtFirstDuplicateInstruction = start.followInstructions.valueAtDuplicate
+  println(s"Part 1: ${valueAtFirstDuplicateInstruction.get}")
   val swapIndexes = instructions.zipWithIndex
-    .filter { case (instruction, index) =>
-      instruction.isInstanceOf[NoOperation] || instruction.isInstanceOf[Jump]
-    }
-    .map(_._2)
+    .filter { case (instruction, _) => instruction.isInstanceOf[NoOperation] || instruction.isInstanceOf[Jump] }
+    .map { case (_, index) => index }
   val possibleInstructions = swapIndexes
     .map(swapIndex => instructions.updated(swapIndex, Instruction.swap(instructions(swapIndex))))
-    .map(swappedInstructions => BootState.from(swappedInstructions).followInstructionsUntilDuplicateOrTerminate)
+    .map(swappedInstructions => BootState.from(swappedInstructions).followInstructions)
     .find(_.valueAtTermination.isDefined)
     .flatMap(_.valueAtTermination)
   println(s"Part 2: ${possibleInstructions.get}")
 
 trait Instruction:
-  def next(bootState: BootState): BootState
+  def follow(bootState: BootState): BootState
 case class NoOperation(value: Int) extends Instruction:
-  override def next(bootState: BootState): BootState = bootState.addToVisited.addToIndex(1)
+  override def follow(bootState: BootState): BootState = bootState.addToIndex(1)
 case class Accumulate(value: Int) extends Instruction:
-  override def next(bootState: BootState): BootState = bootState.addToVisited.addToIndex(1).addToAccumulator(value)
+  override def follow(bootState: BootState): BootState = bootState.addToIndex(1).addToAccumulator(value)
 case class Jump(value: Int) extends Instruction:
-  override def next(bootState: BootState): BootState = bootState.addToVisited.addToIndex(value)
+  override def follow(bootState: BootState): BootState = bootState.addToIndex(value)
 
 object Instruction:
-  def swap(i: Instruction): Instruction = i match
+  def swap(instruction: Instruction): Instruction = instruction match
     case noOperation: NoOperation => Jump(noOperation.value)
     case jump: Jump               => NoOperation(jump.value)
-    case _                        => i
-  def fromString(s: String): Option[Instruction] = s match
+    case _                        => instruction
+  def fromString(string: String): Option[Instruction] = string match
     case s"$operationString $valueString" =>
       operationString match
         case "nop" => Some(NoOperation(valueString.toInt))
@@ -40,24 +38,18 @@ object Instruction:
         case "jmp" => Some(Jump(valueString.toInt))
     case _ => None
 
-case class TerminationValues(valueAtDuplicate: Option[Int], valueAtTermination: Option[Int])
+case class FinalValue(valueAtDuplicate: Option[Int], valueAtTermination: Option[Int])
 
-case class BootState(
-    instructions: Vector[Instruction],
-    instructionIndex: Int,
-    accumulator: Int,
-    instructionsVisited: Set[Int]
-):
-  def addToVisited: BootState             = this.copy(instructionsVisited = instructionsVisited + instructionIndex)
-  def addToIndex(n: Int): BootState       = this.copy(instructionIndex = instructionIndex + n)
-  def addToAccumulator(n: Int): BootState = this.copy(accumulator = accumulator + n)
+case class BootState(instructions: Vector[Instruction], index: Int, accumulator: Int, visited: Set[Int]):
   private def currentInstruction: Option[Instruction] =
-    if instructions.indices.contains(instructionIndex) then Some(instructions(instructionIndex)) else None
-  def followInstructionsUntilDuplicateOrTerminate: TerminationValues =
-    if instructionsVisited.contains(instructionIndex) then TerminationValues(Some(accumulator), None)
-    else
-      currentInstruction match
-        case Some(instruction) => instruction.next(this).followInstructionsUntilDuplicateOrTerminate
-        case None              => TerminationValues(None, Some(accumulator))
+    if instructions.isDefinedAt(index) then Some(instructions(index)) else None
+  private def markVisited: BootState      = this.copy(visited = visited + index)
+  def addToIndex(n: Int): BootState       = this.markVisited.copy(index = index + n)
+  def addToAccumulator(n: Int): BootState = this.copy(accumulator = accumulator + n)
+  def followInstructions: FinalValue = if visited.contains(index) then FinalValue(Some(accumulator), None)
+  else
+    currentInstruction match
+      case Some(instruction) => instruction.follow(this).followInstructions
+      case None              => FinalValue(None, Some(accumulator))
 object BootState:
   def from(instructions: Vector[Instruction]): BootState = BootState(instructions, 0, 0, Set.empty[Int])
