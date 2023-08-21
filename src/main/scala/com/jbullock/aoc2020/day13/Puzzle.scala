78c7963889a42b36fdfd8d3df7eac8c689a13aed
NotJason7 <notjason7@gmail.com>
2023-08-21 19:45:47 +0100

    Day 13 part 2

--- src/main/scala/com/jbullock/aoc2020/day13/Puzzle.scala
+++ src/main/scala/com/jbullock/aoc2020/day13/Puzzle.scala
@@ -1,14 +1,31 @@
 package com.jbullock.aoc2020.day13
 
+import scala.annotation.tailrec
+
 @main def solvePuzzle(): Unit =
   val input        = scala.io.Source.fromResource("aoc/2020/Day13/Input.txt").getLines.toVector
   val earliestTime = input.head.toInt
-  val busses       = input.tail.head.split(",").filterNot(_ == "x").map(i => Bus(i.toInt)).toVector
-//  val busMinDepartures = busses.map(_.departureAfter(earliestTime))
-//  val earliestDepartingBus = busses.minBy(_.departureAfter(earliestTime))
+  val busses = input.tail.head
+    .split(",")
+    .zipWithIndex
+    .filterNot(_._1 == "x")
+    .map { case (id, index) => Bus(id.toInt, index) }
+    .toVector
   val part1 = busses.minBy(_.departureAfter(earliestTime)).waitTimesId(earliestTime)
   println(s"Part 1: $part1")
+  val maxBus = busses.maxBy(_.id)
+  val start  = SyncedBusses(maxBus.id - maxBus.departAt, maxBus.id, Set(maxBus), busses.toSet - maxBus)
+  val part2  = start.syncBusTime
+  println(s"Part 2: $part2")
 
-case class Bus(id: Int):
-  def waitTimesId(minute: Int): Int    = (departureAfter(minute) - minute) * id
-  def departureAfter(minute: Int): Int = (id - (minute % id)) + minute
+case class SyncedBusses(time: Long, delta: Long, synced: Set[Bus], unsynced: Set[Bus]):
+  def next: SyncedBusses =
+    val nextSynced = unsynced.filter(bus => bus.isContestValidAt(time))
+    val nextDelta  = delta * nextSynced.map(_.id).product
+    SyncedBusses(time + nextDelta, nextDelta, synced ++ nextSynced, unsynced -- nextSynced)
+  @tailrec final def syncBusTime: Long = if unsynced.isEmpty then time - delta else next.syncBusTime
+
+case class Bus(id: Long, departAt: Long):
+  def waitTimesId(minute: Long): Long         = (departureAfter(minute) - minute) * id
+  def departureAfter(minute: Long): Long      = (id - (minute       % id)) + minute
+  def isContestValidAt(minute: Long): Boolean = (minute + departAt) % id == 0
