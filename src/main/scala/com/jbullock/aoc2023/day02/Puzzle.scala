1f09b637509a3b03f7311da3ec27513bd6bae205
NotJason7 <notjason7@gmail.com>
2024-01-16 15:31:10 +0000

    Day 2 tidyup

--- src/main/scala/com/jbullock/aoc2023/day02/Puzzle.scala
+++ src/main/scala/com/jbullock/aoc2023/day02/Puzzle.scala
@@ -9,28 +9,22 @@ package com.jbullock.aoc2023.day02
   println(s"Part 2: $part2")
 
 enum Colour:
-  case Red, Blue, Green
+  case red, blue, green
 
 case class Game(id: Int, rounds: Vector[Map[Colour, Int]]):
   def maxOfColourSeen(colour: Colour): Int = rounds.map(_.getOrElse(colour, 0)).max
   def isPossibleGame(reds: Int, greens: Int, blues: Int): Boolean =
-    maxOfColourSeen(Colour.Red) <= reds
-      && maxOfColourSeen(Colour.Blue) <= blues
-      && maxOfColourSeen(Colour.Green) <= greens
-  def fewestCubes: Map[Colour, Int] = Map(
-    Colour.Red   -> maxOfColourSeen(Colour.Red),
-    Colour.Green -> maxOfColourSeen(Colour.Green),
-    Colour.Blue  -> maxOfColourSeen(Colour.Blue)
-  )
-  def power: Int = fewestCubes.values.product
+    maxOfColourSeen(Colour.red) <= reds
+      && maxOfColourSeen(Colour.blue) <= blues
+      && maxOfColourSeen(Colour.green) <= greens
+  def fewestCubes: Map[Colour, Int] = Colour.values.map(colour => colour -> maxOfColourSeen(colour)).toMap
+  def power: Int                    = fewestCubes.values.product
 object Game:
   def fromGameString(gameString: String): Option[Game] = gameString match
     case s"Game $gameIndex: $gameString" =>
       val roundStrings = gameString.split("; ").toVector
-      val cubeNumbers = roundStrings.map(_.split(", ").toVector.map {
-        case s"$quantity red"   => (Colour.Red, quantity.toInt)
-        case s"$quantity blue"  => (Colour.Blue, quantity.toInt)
-        case s"$quantity green" => (Colour.Green, quantity.toInt)
+      val cubeNumbers = roundStrings.map(_.split(", ").toVector.map { case s"$quantity $colorString" =>
+        (Colour.valueOf(colorString), quantity.toInt)
       }.toMap)
       Some(Game(gameIndex.toInt, cubeNumbers))
     case _ => None
