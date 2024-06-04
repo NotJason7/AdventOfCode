ab36c1246e9350b2394f704254c613a61294038f
NotJason7 <notjason7@gmail.com>
2024-06-04 14:58:36 +0100

    Finished day 4 again

--- src/main/scala/com/jbullock/aoc2021_2/day04/Puzzle.scala
+++ src/main/scala/com/jbullock/aoc2021_2/day04/Puzzle.scala
@@ -1,8 +1,10 @@
 package com.jbullock.aoc2021_2.day04
 
+import scala.annotation.tailrec
+
 @main def solvePuzzle(): Unit =
-  val input     = scala.io.Source.fromResource("aoc/2021/Day04/Example.txt").getLines.toSeq
-  val drawOrder = input.head
+  val input     = scala.io.Source.fromResource("aoc/2021/Day04/Input.txt").getLines.toSeq
+  val drawOrder = input.head.split(',').map(_.toInt).toSeq
   val cards =
     input
       .drop(2)
@@ -10,15 +12,32 @@ package com.jbullock.aoc2021_2.day04
       .map(s => s.trimSpaces.split(' ').toSeq.map(_.toInt))
       .grouped(5)
       .map(rows => Card(rows))
-  cards.foreach(_.show())
+      .toSeq
+  val cardsAtWinningDraw = findScoresAtWinningDraw(cards, drawOrder)
+  val part1              = cardsAtWinningDraw.head.score
+  println(s"Part 1: $part1")
+  val part2 = cardsAtWinningDraw.last.score
+  println(s"Part 2: $part2")
+
+def findScoresAtWinningDraw(cards: Seq[Card], draws: Seq[Int]): Seq[Card] =
+
+  @tailrec def loop(currentCards: Seq[Card], drawsLeft: Seq[Int], wonCards: Seq[Card] = Seq.empty[Card]): Seq[Card] =
+    val (newlyWonCards, notYetWonCards) = currentCards.partition(_.hasWon)
+    val nextWonCards                    = wonCards ++ newlyWonCards
+    if notYetWonCards.isEmpty then nextWonCards
+    else
+      val nextCards = notYetWonCards.map(_.mark(drawsLeft.head))
+      loop(nextCards, drawsLeft.tail, nextWonCards)
+
+  loop(cards, draws)
 
 extension (s: String)
   def trimSpaces: String = s.dropWhile(_ == ' ').reverse.dropWhile(_ == ' ').reverse.replaceAll(" +", " ")
 
-extension(rows: Seq[Seq[Int]]) def hasWinningRow(marked: Set[Int]): Boolean = rows.exists(_.forall(marked.contains))
+extension (rows: Seq[Seq[Int]]) def hasWinningRow(marked: Seq[Int]): Boolean = rows.exists(_.forall(marked.contains))
 
-case class Card(rows: Seq[Seq[Int]], marked: Set[Int] = Set.empty[Int]):
+case class Card(rows: Seq[Seq[Int]], marked: Seq[Int] = Seq.empty[Int]):
   private val columns: Seq[Seq[Int]] = rows.transpose
-  def mark(i: Int): Card = this.copy(marked = marked + i)
-  def hasWon: Boolean = rows.hasWinningRow(marked) || columns.hasWinningRow(marked)
-  def show(): Unit = rows.foreach(row => println(row.mkString(",")))
+  def mark(i: Int): Card             = this.copy(marked = marked :+ i)
+  def hasWon: Boolean                = rows.hasWinningRow(marked) || columns.hasWinningRow(marked)
+  def score: Int                     = rows.flatten.filterNot(marked.contains).sum * marked.last
